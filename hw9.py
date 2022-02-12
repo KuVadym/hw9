0506ae09328a8d06a994841e76a8df5975c6d947
KuVadym <97878308+KuVadym@users.noreply.github.com>
2022-02-12 22:23:01 +0200

    Add files via upload

---
+++ hw9.py
@@ -0,0 +1,87 @@
+def hello (dictt, *args, **kwargs):
+    result = "How can I help you?"
+    return result
+
+def all_number(dictt, *args, **kwargs):
+    resalt = dictt
+    return resalt
+
+
+def adds(dictt, name, phone):
+    if name not in dictt.keys():
+        dictt[name] = phone
+        resalt = f"New contact: {name} - {phone}"
+    return resalt
+
+
+def phone(dictt, name, *args, **kwargs):
+    resalt = dictt[name]
+    return resalt
+
+
+def change(dictt, name, phone, *args, **kwargshell):
+    dictt[name] = phone
+    resalt = f"New phone for {name} is: {phone}"
+    return resalt
+
+def finish (dictt, *args, **kwargshell):
+    resalt = "Good bye!"
+    return resalt
+
+dictt = {
+    "ivan": "0-000-00-00",
+    "john": "0-123-45-67"
+}
+def input_error (*args, **kwargshell):
+    pass
+
+def parsing (user_input):
+    xy = user_input.split(" ")
+    try:
+        if xy[1] != "all" and xy[1] != "bye":
+            global name
+            name = xy[1]
+            try:
+                if xy[2] != "":
+                    global num
+                    num = xy [2]
+            except:
+                num = ""
+    except:
+        pass
+    return name, num
+
+name, num = "", ""
+
+
+def handler(operation):
+    parsing (operation)
+    command = {
+        "hello": hello,                            
+        f"phone {name}": phone,                         
+        f"add {name} {num}": adds,                            
+        "show all": all_number,                   
+        f"change {name} {num}": change,                        
+        "good bye": finish,                       
+        "close": finish,                          
+        "exit": finish
+    }
+    resalt = command.get(operation)
+    return resalt
+
+def main():
+    print("User help bot v 0.0.1")
+    while 2 > 1:
+        try:
+            h = input("Enter your command: ")
+            hh = handler(h.lower())
+            h_two = hh(dictt, name, num)
+            print(h_two)
+            if h == "good bye" or h == "close" or h == "exit":
+                break
+        except TypeError:
+            print ("Unknown command")
+
+
+if __name__ == '__main__':
+    main()
